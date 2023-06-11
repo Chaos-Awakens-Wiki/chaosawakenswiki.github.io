59c04fe966118e925a56d1e1f9e8acb9357d7c6a
Blackout03 <gardner.hayden03@gmail.com>
2023-06-12 07:24:11 +1000

    Stuffs
    - Use "variables" for name, health and armor.
    - Fix centering of Entity Information stuff.
    - Updated "change_image.js" to be better.

--- scripts/change_image.js
+++ scripts/change_image.js
@@ -1,65 +1,36 @@
-﻿function changeImage(imgURL, imgAlt, type) {
+﻿let animationItems;
+
+window.addEventListener("DOMContentLoaded", () => {
+	animationItems = {
+		idle: document.getElementById("entity-information-image-selector-item-idle"),
+		awakening: document.getElementById("entity-information-image-selector-item-awakening"),
+		death: document.getElementById("entity-information-image-selector-item-death"),
+		walking: document.getElementById("entity-information-image-selector-item-walking")
+	};
+
+	for (const animationType in animationItems) {
+		if (animationItems.hasOwnProperty(animationType)) {
+			const item = animationItems[animationType];
+			if (item) {
+				item.style.borderBottom = animationType === "idle" ? "lightskyblue 3px solid" : "transparent 3px solid";
+			}
+		}
+	}
+});
+
+function changeImage(imgURL, imgAlt, type) {
 	const image = document.getElementById("entity-information-image");
-	const idle = document.getElementById("entity-information-image-selector-item-idle");
-	const awakening = document.getElementById("entity-information-image-selector-item-awakening");
-	const death = document.getElementById("entity-information-image-selector-item-death");
-	const walking = document.getElementById("entity-information-image-selector-item-walking");
 
 	image.src = imgURL;
 	image.alt = imgAlt;
 
-	console.log(type)
-	if (type === "idle") {
-		if (idle ) {
-			idle.style.borderBottom = "lightskyblue 3px solid"
-		}
-		if (awakening ) {
-			awakening.style.borderBottom = "transparent 3px solid"
-		}
-		if (death ) {
-			death.style.borderBottom = "transparent 3px solid"
-		}
-		if (walking ) {
-			walking.style.borderBottom = "transparent 3px solid"
-		}
-	} else if (type === "awakening") {
-		if (idle ) {
-			idle.style.borderBottom = "transparent 3px solid"
-		}
-		if (awakening ) {
-			awakening.style.borderBottom = "lightskyblue 3px solid"
-		}
-		if (death ) {
-			death.style.borderBottom = "transparent 3px solid"
-		}
-		if (walking ) {
-			walking.style.borderBottom = "transparent 3px solid"
-		}
-	} else if (type === "death") {
-		if (idle ) {
-			idle.style.borderBottom = "transparent 3px solid"
-		}
-		if (awakening ) {
-			awakening.style.borderBottom = "transparent 3px solid"
-		}
-		if (death ) {
-			death.style.borderBottom = "lightskyblue 3px solid"
-		}
-		if (walking ) {
-			walking.style.borderBottom = "transparent 3px solid"
-		}
-	} else if (type === "walking") {
-		if (idle ) {
-			idle.style.borderBottom = "transparent 3px solid"
-		}
-		if (awakening ) {
-			awakening.style.borderBottom = "transparent 3px solid"
-		}
-		if (death ) {
-			death.style.borderBottom = "transparent 3px solid"
-		}
-		if (walking ) {
-			walking.style.borderBottom = "lightskyblue 3px solid"
+	for (const animationType in animationItems) {
+		if (animationItems.hasOwnProperty(animationType)) {
+			const item = animationItems[animationType];
+			const borderStyle = animationType === type ? "lightskyblue 3px solid" : "transparent 3px solid";
+			if (item) {
+				item.style.borderBottom = borderStyle;
+			}
 		}
 	}
 }
---
+++ scripts/replace_variables.js
@@ -0,0 +1,48 @@
+﻿document.addEventListener("DOMContentLoaded", function() {
+	let htmlContent = document.body.innerHTML;
+
+	const title = document.querySelector("title").innerText;
+	const newName = title.replace(" | Chaos Awakens Wiki", "");
+	htmlContent = htmlContent.replace(/\${name}/g, newName);
+
+	const healthIcon = `<img class="stat-icon" src="../images/misc/Heart.svg" alt="Heart">`
+	const halfHealthIcon = `<img class="stat-icon" src="../images/misc/Half_Heart.svg" alt="Heart">`
+	let newHealth;
+	const heartCount = health / 2
+	if (health <= 10) {
+		if (health % 2 === 0) {
+			newHealth = health + " (" + repeatString(heartCount, healthIcon) + ")"
+		} else {
+			newHealth = health + " (" + repeatString(heartCount - 1, healthIcon) + halfHealthIcon + ")"
+		}
+	} else {
+		newHealth = health + " (" + heartCount + " x " + healthIcon + ")"
+	}
+	htmlContent = htmlContent.replace(/\${health}/g, newHealth);
+
+	const armorIcon = `<img class="stat-icon" src="../images/misc/Armor.svg" alt="Armor">`
+	const halfArmorIcon = `<img class="stat-icon" src="../images/misc/Half_Armor.svg" alt="Armor">`
+	let newArmor;
+	const armor = null;
+	const armorCount = armor / 2
+	if (armor <= 10) {
+		if (armor % 2 === 0) {
+			newArmor = armor + " (" + repeatString(armorCount, armorIcon) + ")"
+		} else {
+			newArmor = armor + " (" + repeatString(armorCount - 1, armorIcon) + halfArmorIcon + ")"
+		}
+	} else {
+		newArmor = armor + " (" + armorCount + " x " + armorIcon + ")"
+	}
+	htmlContent = htmlContent.replace(/\${armor}/g, newArmor);
+
+	document.body.innerHTML = htmlContent
+});
+
+function repeatString(count, repeatedValue) {
+	let result = "";
+	for (let i = 0; i < count; i++) {
+		result += repeatedValue;
+	}
+	return result;
+}
