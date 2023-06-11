960f17a171c191d6917e6c3f8d25d5aaef998aa2
Blackout03 <gardner.hayden03@gmail.com>
2023-06-12 00:35:17 +1000

    Many Images/Gifs and Stuff
    - All the images and gifs transferred from fandom site.
    - Added Hercules Beetle Page.
    - Fixed bunch of stuff on Apple Cow and Crystal Apple Cow pages.
    - Update some CSS and JS stuffs.

--- scripts/change_image.js
+++ scripts/change_image.js
@@ -1,6 +1,8 @@
 ﻿function changeImage(imgURL, imgAlt, type) {
 	const image = document.getElementById("entity-information-image");
 	const idle = document.getElementById("entity-information-image-selector-item-idle");
+	const awakening = document.getElementById("entity-information-image-selector-item-awakening");
+	const death = document.getElementById("entity-information-image-selector-item-death");
 	const walking = document.getElementById("entity-information-image-selector-item-walking");
 
 	image.src = imgURL;
@@ -8,10 +10,56 @@
 
 	console.log(type)
 	if (type === "idle") {
-		idle.style.borderBottom = "lightskyblue 3px solid"
-		walking.style.borderBottom = "transparent 3px solid"
+		if (idle ) {
+			idle.style.borderBottom = "lightskyblue 3px solid"
+		}
+		if (awakening ) {
+			awakening.style.borderBottom = "transparent 3px solid"
+		}
+		if (death ) {
+			death.style.borderBottom = "transparent 3px solid"
+		}
+		if (walking ) {
+			walking.style.borderBottom = "transparent 3px solid"
+		}
+	} else if (type === "awakening") {
+		if (idle ) {
+			idle.style.borderBottom = "transparent 3px solid"
+		}
+		if (awakening ) {
+			awakening.style.borderBottom = "lightskyblue 3px solid"
+		}
+		if (death ) {
+			death.style.borderBottom = "transparent 3px solid"
+		}
+		if (walking ) {
+			walking.style.borderBottom = "transparent 3px solid"
+		}
+	} else if (type === "death") {
+		if (idle ) {
+			idle.style.borderBottom = "transparent 3px solid"
+		}
+		if (awakening ) {
+			awakening.style.borderBottom = "transparent 3px solid"
+		}
+		if (death ) {
+			death.style.borderBottom = "lightskyblue 3px solid"
+		}
+		if (walking ) {
+			walking.style.borderBottom = "transparent 3px solid"
+		}
 	} else if (type === "walking") {
-		idle.style.borderBottom = "transparent 3px solid"
-		walking.style.borderBottom = "lightskyblue 3px solid"
+		if (idle ) {
+			idle.style.borderBottom = "transparent 3px solid"
+		}
+		if (awakening ) {
+			awakening.style.borderBottom = "transparent 3px solid"
+		}
+		if (death ) {
+			death.style.borderBottom = "transparent 3px solid"
+		}
+		if (walking ) {
+			walking.style.borderBottom = "lightskyblue 3px solid"
+		}
 	}
 }
