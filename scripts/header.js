a9f6f02776b991f38a4918b95fbb17557a4b06ba
Blackout03 <gardner.hayden03@gmail.com>
2024-04-07 21:18:32 +1000

    Updated the Header to have Nav

---
+++ scripts/header.js
@@ -0,0 +1,28 @@
+﻿window.onload = () => {
+	const hamburger = document.querySelector(".hamburger");
+	const nav = document.querySelector(".nav-dropdown");
+	
+	const isDescendant = (parent, child) => {
+		let node = child.parentNode;
+		while (node != null) {
+			if (node === parent) {
+				return true;
+			}
+			node = node.parentNode;
+		}
+		return false;
+	};
+	
+	document.body.addEventListener("click", (event) => {
+		if (!event.target.classList.contains("hamburger") && !isDescendant(nav, event.target)) {
+			hamburger.classList.remove("active");
+			nav.classList.remove("active");
+		}
+	});
+
+	hamburger.addEventListener("click", () => {
+		event.stopPropagation();
+		hamburger.classList.toggle("active")
+		nav.classList.toggle("active")
+	})
+};
